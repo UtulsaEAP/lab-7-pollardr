609aa3ea8ec63ed2e0285a3ac71a1e5989037e1b
rileyspollard <rileyspollard@outlook.com>
2024-03-06 13:39:35 -0600

    Problem 2

--- Problem 2/wordInRange.py
+++ Problem 2/wordInRange.py
@@ -3,6 +3,19 @@
 
 def wordInRange():
     #Type your code here
-    return
+    filename = input()
+    string1 = input() #string 1 is lower bound
+    string2 = input() #string 2 is higher bound
+
+    with open(filename, 'r') as myfile:
+        lines = myfile.readlines()
+
+    for eachline in lines:
+        word = eachline.strip()
+        if string1 <= word and word <= string2:
+            print(word + ' - in range')
+        else:
+            print(word + ' - not in range')
+    
 if __name__ == '__main__':
     wordInRange()
