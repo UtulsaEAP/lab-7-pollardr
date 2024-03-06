f616bbdda930fed5d509bf0fa084e3bfe36641e6
rileyspollard <rileyspollard@outlook.com>
2024-03-06 13:23:51 -0600

    Problem 1

--- Problem 1/exceptionHandling.py
+++ Problem 1/exceptionHandling.py
@@ -8,9 +8,12 @@ def exceptionHandling():
     while name != '-1':
         # FIXME: The following line will throw ValueError exception.
         #        Insert try/except blocks to catch the exception.
-        age = int(parts[1]) + 1
-        print(f'{name} {age}')
-        
+        try:
+            age = int(parts[1]) + 1
+            print(f'{name} {age}')
+        except ValueError: 
+            print(f'{name} 0')
+            
         # Get next line
         parts = input().split()
         name = parts[0]
