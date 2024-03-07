296eba2d736ba2a6cf70b5bc6e8182bc4337e89b
rileyspollard <rileyspollard@outlook.com>
2024-03-07 15:11:44 -0600

    missing underscore

--- Problem 4/fileNameChange.py
+++ Problem 4/fileNameChange.py
@@ -5,9 +5,9 @@ def fileNameChange():
     #type your code here
     filepath = input().strip()
 
-    with open(filepath, "r") as file:
+    with open(filepath,"r") as file:
         for line in file:
-            modifiedfile = line.strip().replace('_photo.jpg', 'info.txt')
+            modifiedfile = line.strip().replace('_photo.jpg', '_info.txt')
             print(modifiedfile)
     return
 
